2b7f7724d8a7bf4541b8103a7e01ff17eafc0d30
durumi-contact <durumi.contact@gmail.com>
2024-06-12 11:28:39 +0900

    Update create.js

--- js/create.js
+++ js/create.js
@@ -1,15 +1,10 @@
-document.getElementById('changeButton').addEventListener('click', function() {
-  var container = document.getElementById('container');
-  var heading = document.getElementById('heading');
-  
-  // 기존 요소 삭제
-  container.removeChild(heading);
+document.getElementById('privatePolicyNavItem').addEventListener('click', function() {
+  // 페이지의 모든 요소를 지우는 코드 작성
+  var allElements = document.getElementsByTagName("*");
+  for (var i = 0; i < allElements.length; i++) {
+    allElements[i].parentNode.removeChild(allElements[i]);
+  }
 
-  // 새로운 텍스트 요소 생성
-  var newHeading = document.createElement('h1');
-  newHeading.innerText = '새로운 텍스트 요소';
-  newHeading.id = 'heading';
-  
-  // 컨테이너에 새로운 요소 추가
-  container.appendChild(newHeading);
+  // 예를 들어, 모든 요소를 삭제한 후 새로운 내용을 추가하거나 다른 동작을 수행할 수 있습니다.
+  // 여기에 코드를 추가하세요.
 });
