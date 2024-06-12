72905d410627411587ffbfae4077173d5b1ea657
durumi-contact <durumi.contact@gmail.com>
2024-06-12 11:34:57 +0900

    Update create.js

--- js/create.js
+++ js/create.js
@@ -1,10 +1,8 @@
-document.getElementById('privatePolicyNavItem').addEventListener('click', function() {
-  // 페이지의 모든 요소를 지우는 코드 작성
-  var allElements = document.getElementsByTagName("*");
-  for (var i = 0; i < allElements.length; i++) {
-    allElements[i].parentNode.removeChild(allElements[i]);
-  }
-
-  // 예를 들어, 모든 요소를 삭제한 후 새로운 내용을 추가하거나 다른 동작을 수행할 수 있습니다.
-  // 여기에 코드를 추가하세요.
+$(document).ready(function(){
+    // privatepolicy 버튼 클릭 이벤트
+    $("#privatePolicyNavItem a").click(function(event){
+        event.preventDefault(); // 기본 동작 방지
+        var url = "privatepolicy.html"; // 로드할 페이지 URL
+        $("#content").load(url); // privatepolicy.html 페이지의 내용을 로드하여 content 요소에 표시
+    });
 });
